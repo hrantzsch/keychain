c5daba4161f29cefdd15f5feaee2a24d3ea42601
Hannes Rantzsch <mail@hannesrantzsch.de>
2019-11-15 17:47:38 +0700

    another API proposal (does not compile)

--- keychain.h
+++ keychain.h
@@ -24,37 +24,102 @@ class Keychain {
                                  const std::string &user);
 
    public:
-    /*! \brief Construct a Keychain specific to a service/user combination
+    /*! \brief Construct a Keychain object
      *
-     * \param service Identifier for the service whose password should be stored
-     *                or updated. Note that a platform specific service name
-     *                will be used based on the provided identifier.
-     *                \sa getServiceName
-     * \param user
+     * \param package Package identifier corresponding to the format
+     *                com.example.application. On MacOS this identifier is pre-
+     *                pended to the service name and is visible to users. On
+     *                Linux it is used as the schema name and is normally not
+     *                visible to users. On Windows this parameter is ignored.
      */
-    Keychain(const std::string &service, const std::string &user);
+    Keychain(const std::string &package);
 
-    std::optional<std::string> getPassword();
-    bool setPassword(const std::string &password);
-    bool deletePassword();
+    /*! \brief Get a password
+     *
+     * \param service Identifier for the service whose passwords should be
+     * stored or updated.
+     *
+     * \param username Username or account name that the stored or updated
+     *                 passwords belong to.
+     */
+    std::optional<std::string> getPassword(const std::string &service,
+                                           const std::string &username);
 
-    //! Return the error resulting from the most recent call to getPassword,
-    //! addPassword, or deletePassword, if any
+    /*! \brief Update a password
+     *
+     * \param password The password to be stored.
+     *
+     * \param service Identifier for the service whose passwords should be
+     * stored or updated.
+     *
+     * \param username Username or account name that the stored or updated
+     *                 passwords belong to.
+     */
+    bool setPassword(const std::string &password, const std::string &service,
+                     const std::string &username);
+
+    /*! \brief Delete a password
+     *
+     * \param service Identifier for the service whose passwords should be
+     * stored or updated.
+     *
+     * \param username Username or account name that the stored or updated
+     *                 passwords belong to.
+     */
+    bool deletePassword(const std::string &service,
+                        const std::string &username);
+
+    /*! \brief Get the last error, if any
+     *
+     * Return the error resulting from the most recent call to getPassword,
+     * addPassword, or deletePassword. If the last call to any of the functions
+     * did not result in an error, std::nullopt is returned.
+     */
     std::optional<std::string> lastError() const;
+
+   protected:
+    void clearError();
 
     template <typename F>
     bool updatePassword(F &&updateFun);
 
-   private:
-    //! \brief Create a platform specific service identifier
-    std::string makeServiceName(const std::string &service) const;
-
-    void clearError();
-
-   private:
-    std::string _service;
-    std::string _user;
+   protected:
+    std::string _package;
     std::optional<std::string> _lastError;
+};
+
+/*! \brief Convenience utility for accessing only one specific service and
+ *         account in a Keychain.
+ */
+class Account : public Keychain {
+   public:
+    /*! \brief Construct a Account specific to a service/username combination
+     *
+     * \param service Identifier for the service whose passwords should be
+     *                stored or updated.
+     *
+     * \param username Username or account name that the stored or updated
+     *                 passwords belong to.
+     *
+     * \param package Package identifier corresponding to the format
+     *                com.example.application. On MacOS this identifier is pre-
+     *                pended to the service name and is visible to users. On
+     *                Linux it is used as the schema name and is normally not
+     *                visible to users. On Windows this parameter is ignored.
+     */
+    Account(const std::string &package, const std::string &service,
+            const std::string &username);
+
+    //! \brief Get the password
+    std::optional<std::string> getPassword();
+    //! \brief Update the password
+    bool setPassword(const std::string &password);
+    //! \brief Delete the password
+    bool deletePassword();
+
+   protected:
+    std::string _service;
+    std::string _username;
 };
 
 }  // namespace keychain
